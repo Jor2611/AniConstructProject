35527c5a9c67a2d4c22fc54e506ac854791bdc29
Jora <khachatryanj007@gmail.com>
2021-02-07 21:51:02 +0400

    Updates

--- js/custom.js
+++ js/custom.js
@@ -287,6 +287,7 @@ jQuery(document).ready(function($) {
 		let path = window.location.pathname.replace('/dist/','');
 		path = path.replace('_ru','');
 		path = path.replace('.html','');
+		path.langth < 1 ? path = 'index' : path;
 		window.location.replace(`${path}_am.html`);
 		// console.log(window.location.pathname.match(/am/i));
 	});
@@ -295,6 +296,7 @@ jQuery(document).ready(function($) {
 		let path = window.location.pathname.replace('/dist/','');
 		path = path.replace('_ru','');
 		path = path.replace('_am','');
+		path.langth < 1 ? path = 'index' : path;
 		window.location.replace(`${path}`);
 	});
 
@@ -302,6 +304,7 @@ jQuery(document).ready(function($) {
 		let path = window.location.pathname.replace('/dist/','');
 		path = path.replace('_am','');
 		path = path.replace('.html','');
+		path.langth < 1 ? path = 'index' : path;
 		window.location.replace(`${path}_ru.html`);
 	});
 });
